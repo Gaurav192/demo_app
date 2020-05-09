b536633d7d8aac2b1810e12c9d156589a886c17e
Gaurav192 <gauravshetty96@gmail.com>
2020-05-09 18:25:30 +0530

    Add header.

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -10,219 +10,248 @@ class _HomePageState extends State<HomePage> {
   bool hideFab = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: ListView(
-        padding: EdgeInsets.all(16),
-        children: <Widget>[
-          Container(
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  border: Border.all(width: 1, color: Colors.grey[300])),
+    return SafeArea(
+      top: true,
+      bottom: true,
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        body: ListView(
+          padding: EdgeInsets.all(16),
+          children: <Widget>[
+            Card(
               child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
-                  Stack(
-                    children: <Widget>[
-                      CircleAvatar(
-                        backgroundColor: Colors.grey[300],
-                        radius: 60,
-                        child: Image.asset('assets/placeholder.png'),
-                      ),
-                      Positioned(
-                        right: 2,
-                        bottom: 16,
-                        child: Image.asset(
-                          "assets/camera.png",
-                          height: 24,
-                          width: 24,
-                        ),
-                      ),
-                    ],
+                  Icon(Icons.location_on),
+                  Flexible(
+                    child: Text(
+                      'Vasai, Mumbai, Maharashtra',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText1
+                          .copyWith(color: Colors.black),
+                    ),
                   ),
-                  Column(
-                    children: <Widget>[
-                      Text(
-                        'Jennifer Wilson',
-                        style: Theme.of(context)
-                            .textTheme
-                            .headline6
-                            .copyWith(color: Colors.grey),
-                      ),
-                      SizedBox(height: 16),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          Image.asset(
-                            "assets/location.png",
-                            height: 24,
-                            width: 24,
-                          ),
-                          SizedBox(width: 8),
-                          Text('Mumbai, India')
-                        ],
-                      ),
-                      SizedBox(height: 16),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          Image.asset(
-                            "assets/calendar.png",
-                            height: 24,
-                            width: 24,
-                          ),
-                          SizedBox(width: 8),
-                          Text('12-Apr-1994')
-                        ],
-                      )
-                    ],
-                  )
+                  IconButton(icon: Icon(Icons.edit), onPressed: () {}),
+                  IconButton(
+                      icon: Image.asset("assets/help.png"), onPressed: () {}),
+                  IconButton(icon: Icon(Icons.menu), onPressed: () {})
                 ],
-              )),
-          SizedBox(height: 20),
-          Text(
-            'Health Advisor',
-            style: Theme.of(context).textTheme.headline6,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            height: 150,
-            child: ListView.builder(
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) => Card(
-                  borderOnForeground: true,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8)),
-                  child: Container(
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    border: Border.all(width: 1, color: Colors.grey[300])),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Stack(
+                      children: <Widget>[
+                        CircleAvatar(
+                          backgroundColor: Colors.grey[300],
+                          radius: 60,
+                          child: Image.asset('assets/placeholder.png'),
+                        ),
+                        Positioned(
+                          right: 2,
+                          bottom: 16,
+                          child: Image.asset(
+                            "assets/camera.png",
+                            height: 24,
+                            width: 24,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(
+                          'Jennifer Wilson',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline6
+                              .copyWith(color: Colors.grey),
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Image.asset(
+                              "assets/location.png",
+                              height: 24,
+                              width: 24,
+                            ),
+                            SizedBox(width: 8),
+                            Text('Mumbai, India')
+                          ],
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Image.asset(
+                              "assets/calendar.png",
+                              height: 24,
+                              width: 24,
+                            ),
+                            SizedBox(width: 8),
+                            Text('12-Apr-1994')
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                )),
+            SizedBox(height: 20),
+            Text(
+              'Health Advisor',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 150,
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) => Card(
+                    borderOnForeground: true,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    child: Container(
+                      padding: EdgeInsets.all(16),
+                      width: MediaQuery.of(context).size.width * 0.7,
+                      child: Row(
+                        children: <Widget>[
+                          Flexible(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text(
+                                    'From treatment guidance to post hospitalization care'),
+                                RaisedButton(
+                                    onPressed: () {},
+                                    child: Text(
+                                      'Get in touch',
+                                      style: TextStyle(color: Colors.white),
+                                    ))
+                              ],
+                            ),
+                          ),
+                          Image.asset("assets/advisor.png")
+                        ],
+                      ),
+                    )),
+                itemCount: 4,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Health Rewards',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text('test'),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Explore our Insurance Products',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ListView.builder(
+                padding: EdgeInsets.all(0),
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.symmetric(vertical: 16, horizontal: 4),
                     padding: EdgeInsets.all(16),
-                    width: MediaQuery.of(context).size.width * 0.7,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(width: 1, color: Colors.grey[300])),
                     child: Row(
                       children: <Widget>[
+                        Image.asset("assets/insurance.png"),
+                        SizedBox(
+                          width: 10,
+                        ),
                         Flexible(
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            mainAxisSize: MainAxisSize.min,
                             children: <Widget>[
                               Text(
-                                  'From treatment guidance to post hospitalization care'),
-                              RaisedButton(
-                                  onPressed: () {},
-                                  child: Text(
-                                    'Get in touch',
-                                    style: TextStyle(color: Colors.white),
-                                  ))
+                                'Health Insurance redefined!',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline6
+                                    .copyWith(color: Colors.grey),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Flexible(
+                                fit: FlexFit.loose,
+                                child: Text(
+                                    'Lorem ipsum dolor sitamet is a placeholder text....'),
+                              )
                             ],
                           ),
-                        ),
-                        Image.asset("assets/advisor.png")
+                        )
                       ],
                     ),
-                  )),
-              itemCount: 4,
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            'Health Rewards',
-            style: Theme.of(context).textTheme.headline6,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text('test'),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            'Explore our Insurance Products',
-            style: Theme.of(context).textTheme.headline6,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          ListView.builder(
-              padding: EdgeInsets.all(0),
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemBuilder: (context, index) {
-                return Container(
-                  margin: EdgeInsets.symmetric(vertical: 16, horizontal: 4),
-                  padding: EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      border: Border.all(width: 1, color: Colors.grey[300])),
-                  child: Row(
-                    children: <Widget>[
-                      Image.asset("assets/insurance.png"),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Flexible(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                            Text(
-                              'Health Insurance redefined!',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headline6
-                                  .copyWith(color: Colors.grey),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Flexible(
-                              fit: FlexFit.loose,
-                              child: Text(
-                                  'Lorem ipsum dolor sitamet is a placeholder text....'),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                );
+                  );
+                },
+                itemCount: 20)
+          ],
+        ),
+        bottomNavigationBar: FABBottomAppBar(
+          selectedColor: Colors.black,
+          color: Colors.grey,
+          onTabSelected: _selectedTab,
+          items: [
+            _BottomAppBarItem(
+                assetName: "assets/policy.png", text: 'My Policies'),
+            _BottomAppBarItem(
+                assetName: "assets/locate.png", text: 'Locate Hospital'),
+            _BottomAppBarItem(
+                assetName: "assets/claim.png", text: 'Raise Claim'),
+            _BottomAppBarItem(
+                assetName: "assets/book.png", text: 'Book Services'),
+          ],
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: Builder(
+          builder: (context) => Visibility(
+            visible: !hideFab,
+            child: FloatingActionButton(
+              onPressed: () {
+                _settingModalBottomSheet(context);
+                hideFloatingActionButton(true);
               },
-              itemCount: 20)
-        ],
-      ),
-      bottomNavigationBar: FABBottomAppBar(
-        selectedColor: Colors.black,
-        color: Colors.grey,
-        onTabSelected: _selectedTab,
-        items: [
-          _BottomAppBarItem(
-              assetName: "assets/policy.png", text: 'My Policies'),
-          _BottomAppBarItem(
-              assetName: "assets/locate.png", text: 'Locate Hospital'),
-          _BottomAppBarItem(assetName: "assets/claim.png", text: 'Raise Claim'),
-          _BottomAppBarItem(
-              assetName: "assets/book.png", text: 'Book Services'),
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: Builder(
-        builder: (context) => Visibility(
-          visible: !hideFab,
-          child: FloatingActionButton(
-            onPressed: () {
-              _settingModalBottomSheet(context);
-              hideFloatingActionButton(true);
-            },
-            backgroundColor: Colors.white,
-            tooltip: 'Home',
-            child: Image.asset(
-              "assets/home.png",
-              height: 36,
+              backgroundColor: Colors.white,
+              tooltip: 'Home',
+              child: Image.asset(
+                "assets/home.png",
+                height: 36,
+              ),
+              elevation: 2.0,
             ),
-            elevation: 2.0,
           ),
         ),
       ),
